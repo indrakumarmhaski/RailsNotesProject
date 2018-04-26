1293e97b4f83ec384543e475aefa385dd5b200e3
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-04-26 20:55:42 +0530

    Migrated the database for notes

--- db/migrate/20180426150938_create_notes.rb
+++ db/migrate/20180426150938_create_notes.rb
@@ -1,7 +1,9 @@
 class CreateNotes < ActiveRecord::Migration[5.2]
   def change
     create_table :notes do |t|
-
+      t.string :Title
+      t.text :Body
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_26_150201) do
+ActiveRecord::Schema.define(version: 2018_04_26_150938) do
+
+  create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "Title"
+    t.text "Body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "UserName"
